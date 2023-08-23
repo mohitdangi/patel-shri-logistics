b735d7e76cbc05401a6f41ec3d07dfd76fe505f0
Mohit Dangi <91773940+mohitdangi@users.noreply.github.com>
2023-08-23 15:30:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,14 +16,30 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+//call
+document.getElementById("book-now-button").addEventListener("click", function() {
+    window.location.href = "tel:6260036264";
+});
+
 //whatsapp
 
 document.getElementById("get-quote-button").addEventListener("click", function() {
-    var phoneNumber = "6260036364"; // Replace with the actual phone number
+    var phoneNumber = "6260036264"; // Replace with the actual phone number
     var instantQuoteMessage = "Thank you for requesting an instant quote! Please tell the location from where to pick and drop our Package";
 
     var encodedMessage = encodeURIComponent(instantQuoteMessage);
     var whatsappUrl = "https://wa.me/" + phoneNumber + "?text=" + encodedMessage;
+
+    window.open(whatsappUrl, "_blank");
+});
+
+document.getElementById("send-whatsapp").addEventListener("click", function() {
+    var pickup = document.getElementById("pickupInput").value;
+    var drop = document.getElementById("dropInput").value;
+
+    var message = encodeURIComponent("Hi, I want to make a booking. Pickup: " + pickup + ". Drop: " + drop);
+
+    var whatsappUrl = "https://wa.me/6260036264/?text=" + message;
 
     window.open(whatsappUrl, "_blank");
 });
